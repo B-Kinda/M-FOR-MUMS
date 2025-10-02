2ce57593df053f47a4fd1282085a45497a00d6a3
B-Kinda <baptiste.lebreton97@gmail.com>
2025-10-02 17:12:12 +0200

    commit

---
+++ Projet/index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import router from "./app/routes/index.route.js";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./app/views");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static("./app/public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+app.listen(port, () => {
+	console.log(`Server running on port http://localhost:${port}`);
+});
